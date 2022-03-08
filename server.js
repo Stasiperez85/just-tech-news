4bce050e4f6834d9a87c6b9a69eeee4a68b4ddf3
Anastasia Perez <Stasiperez85@gmail.com>
2022-03-07 21:36:50 -0700

    updated

--- server.js
+++ server.js
@@ -15,7 +15,3 @@ app.use(routes);
 sequelize.sync({ force: false }).then(() => {
     app.listen(PORT, () => console.log('Now listening'));
 });
-
-app.listen(PORT, () => {
-    console.log(`API server now on port ${PORT}!`);
-});
